8398854f455abcd942ba13d3617448132ed9231a
Davi Machado <brisolo@proton.me>
2025-04-19 17:46:22 -0300

    adds cat images as placeholders as per qiracy request

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,53 +1,62 @@
-import type { Actions } from "./$types";
-import { fail, redirect } from "@sveltejs/kit";
-import type { RecordModel } from "pocketbase";
+import type { Actions } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import fs from 'fs/promises';
+import path from 'path';
 
 export const actions = {
-    register: async ({ request, locals }) => {
-        const formData = await request.formData();
-        const data = Object.fromEntries([...formData]);
+	register: async ({ request, locals }) => {
+		const formData = await request.formData();
+		const data = Object.fromEntries([...formData]);
 
-        if (formData === undefined) return fail(400, { missing: true });
-        if (data.password !== data.passwordConfirm) return fail(400, { 
-            error: true,
-            match: false
-        })
+		if (formData === undefined) return fail(400, { missing: true });
+		if (data.password !== data.passwordConfirm)
+			return fail(400, {
+				error: true,
+				match: false
+			});
 
-        try {
-            const reqData = {
-                password: data.password,
-                passwordConfirm: data.passwordConfirm,
-                username: data.username,
-                email: `${data.email}`
-            }
+		try {
+			const randNum = Math.floor(Math.random() * 5);
 
-            const user = await locals.pb
-                .collection("users")
-                .create(reqData)
+			const imagePath = path.resolve(`src/assets/${randNum}.jpg`);
+			const imageBuf = await fs.readFile(imagePath);
 
-            await locals.pb
-                .collection("games")
-                .create({
-                    owner: user.id,
-                    games: null
-                })
+			const imageBlob = new Blob([imageBuf]);
+			const imageFile = new File([imageBlob], `${randNum}.jpg`, {
+				type: 'image/jpeg'
+			});
 
-            locals.pb.authStore.clear(); // forces the user to sign in
-        } catch (err: any) {
-            console.log(err.response.data);
+			const reqData = {
+				password: data.password,
+				passwordConfirm: data.passwordConfirm,
+				username: data.username,
+				email: `${data.email}`,
+				avatar: imageFile
+			};
 
-            return fail(400, {   
-                error: true,
-                incorrect: true,
-                message: err.response.data
-            })
-        } 
+			const user = await locals.pb.collection('users').create(reqData);
 
-        return redirect(303, "/login");
-    }
-} satisfies Actions
+			await locals.pb.collection('games').create({
+				owner: user.id,
+				games: null
+			});
+
+			locals.pb.authStore.clear(); // forces the user to sign in
+		} catch (err: any) {
+			console.log(err.response.data);
+
+			return fail(400, {
+				error: true,
+				incorrect: true,
+				message: err.response.data
+			});
+		}
+
+		return redirect(303, '/login');
+	}
+} satisfies Actions;
 
 interface LoginForm {
-    identity: string,
-    password: string
+	identity: string;
+	password: string;
 }
